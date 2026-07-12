9ed0401eacb2ebb9fc9a3acab0a13337b4847aff
ctkrug <ctkrug4501@gmail.com>
2026-07-12 00:11:58 +0000

    chore: add eslint flat config
    Ignores the vendored jsPDF bundle and scopes browser/Node globals per
    directory (src = browser, test/scripts = Node).

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+
+export default [
+  { ignores: ['src/vendor/**'] },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        process: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['scripts/**/*.js'],
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+  },
+];
